05418db054fa5367a06899a0c77ba7fcb3697304
Efim Zabarsky <zzfima@gmail.com>
2022-10-29 14:28:22 +0300

    add kardianos

--- main.go
+++ main.go
@@ -2,13 +2,47 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/kardianos/service"
 )
 
-func main() {
-	PrintHello()
+const serviceName = "Medium service"
+const serviceDescription = "Simple service, just for fun"
+
+type program struct{}
+
+func (p program) Start(s service.Service) error {
+	fmt.Println(s.String() + " started")
+	go p.run()
+	return nil
 }
 
-// PrintHello print test message
-func PrintHello() {
-	fmt.Println("Hello")
+func (p program) Stop(s service.Service) error {
+	fmt.Println(s.String() + " stopped")
+	return nil
+}
+
+func (p program) run() {
+	for {
+		fmt.Println("Service is running")
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	serviceConfig := &service.Config{
+		Name:        serviceName,
+		DisplayName: serviceName,
+		Description: serviceDescription,
+	}
+	prg := &program{}
+	s, err := service.New(prg, serviceConfig)
+	if err != nil {
+		fmt.Println("Cannot create the service: " + err.Error())
+	}
+	err = s.Run()
+	if err != nil {
+		fmt.Println("Cannot start the service: " + err.Error())
+	}
 }
